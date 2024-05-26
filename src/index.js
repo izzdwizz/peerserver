e13cd1dabefcb0f630e8dadf4593bfdce3a28859
Izuchukwu Uchendu <izuchukwu.uchendu@gmail.com>
2024-05-26 12:06:19 +0100

    server update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
+// import { PeerServer } from 'peer';
+
+// export default function StartPeerServer() {
+// 	PeerServer({ port: 4404, path: '/myapp' });
+// }
+
 import { PeerServer } from 'peer';
 
+// Use the PORT environment variable provided by Render
+const PORT = process.env.PORT || 4404;
+const PATH = '/myapp';
+
 export default function StartPeerServer() {
-	PeerServer({ port: 4404, path: '/myapp' });
+	PeerServer({ port: PORT, path: PATH });
 }
